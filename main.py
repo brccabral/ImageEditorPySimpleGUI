4c8742c934b21569b8e53e75b066d93c36998417
Bruno Cabral <cabral.brc@gmail.com>
2022-04-05 18:55:11 -0700

    added control options

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import PySimpleGUI as sg
 
-control_layout = sg.Column([[]])
+
+control_layout = sg.Column(
+    [
+        [
+            sg.Frame(
+                "Blur",
+                layout=[[sg.Slider(range=(0, 10), orientation="h", key="-BLUR-")]],
+            )
+        ],
+        [
+            sg.Frame(
+                "Contrast",
+                layout=[[sg.Slider(range=(0, 10), orientation="h", key="-CONTRAST-")]],
+            )
+        ],
+        [
+            sg.Checkbox("Emboss", key="-EMBOSS-"),
+            sg.Checkbox("Contour", key="-CONTOUR-"),
+        ],
+        [sg.Checkbox("Flip x", key="-FLIPX-"), sg.Checkbox("Flip y", key="-FLIPY-")],
+        [sg.Button("Save image", key="-SAVE-")],
+    ]
+)
 image_layout = sg.Column([[sg.Image("bird.png")]])
 
-layout = [[image_layout]]
+layout = [[control_layout, image_layout]]
 
 window = sg.Window("Image Editor", layout)
 
